6147136da72c4d870a3f67a011de3dba43d9ce02
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-04 13:48:29 +0530

    Adding greeting message for Address Book

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program.");
+
+            //UC1_FirstPerson person = new UC1_FirstPerson();
+            //person.FirstPerson();
+
+            //EditDetail editDetail = new EditDetail();
+            //editDetail.EditAddressBook();
+
+            ModifyPrebviousDetails.GetCustomer();
+            ModifyPrebviousDetails.ListingPeople();
+
+            
+        }
+    }
+}
